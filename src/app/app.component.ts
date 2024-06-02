095206637e34f34278ce5f861147001695eb6d16
SahenJavac <sahen92developer@gmail.com>
2024-06-02 19:54:37 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DiaryComponent } from './diary/diary.component';
+import { HeaderComponent } from "./header/header.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, DiaryComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'my-diary';
+
+  public testMethod(){
+    console.log("Testing this shit")
+  }
+
+
 }
