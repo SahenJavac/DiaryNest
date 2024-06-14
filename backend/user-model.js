ed5817b4e494aab781f79c8e5215d8d915f0f1d8
SahenJavac <sahen92developer@gmail.com>
2024-06-15 01:39:08 +0530

    user model update

--- backend/user-model.js
+++ backend/user-model.js
@@ -1,11 +1,11 @@
-/* const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model("UserModel", userSchema); */
+module.exports = mongoose.model("UserModel", userSchema); 
